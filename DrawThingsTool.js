9088c0b7d2f722d71111bc0947daeba5a8641f76
Ben Davies <bendavies@he3.com.au>
2024-05-28 15:22:54 +1000

    Refactor configuration logic and add config command

--- DrawThingsTool.js
+++ DrawThingsTool.js
@@ -1,6 +1,7 @@
 const { Command } = require('commander');
 const program = new Command();
 const generate = require('./lib/commands/generate');
+const configCommand = require('./lib/commands/config');
 
 program
   .name('DrawThingsTools')
@@ -18,6 +19,16 @@ program
     generate.generateImages(options.limit, options.preview, options.project, options.prompt)
       .then(() => console.log("All image generations are completed."))
       .catch(error => console.error("Error in generating images:", error));
+  });
+
+program
+  .command('config')
+  .description('Display the current active configuration')
+  .option('-j, --project <path>', 'Path to the project configuration file')
+  .option('-r, --prompt <string>', 'Override base prompt')
+  .action((options) => {
+    configCommand.displayConfig(options)
+      .catch(error => console.error("Error displaying configuration:", error));
   });
 
 program.parse(process.argv);
