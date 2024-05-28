e95eb7c6ca6c70f23d84c81f58da71d49d6a3e45
Ben Davies <bendavies@he3.com.au>
2024-05-29 00:11:25 +1000

    Numerous bug fixes. Mostly working?

--- placeholders.js
+++ placeholders.js
@@ -1,6 +1,18 @@
 //placeholders.js
 module.exports = {
-    hair_color: ["blond", "brown", "red"],
+
+    //hair color
+    hair_color: ["auburn","blond", "brown", "red","black"],
+    
+    //emotion, adjective
     emotion: ["happy", "sad", "angry", "scared", "surprised"],
-    name: ["Mille", "Kelly", "Anna"]
+    
+    //female given names
+    fName: ["Mille", "Kelly", "Anna"]
+
+    //male given names
+    mName: ["Tom", "Fred", "Ben"]
+
+    //family or surnames
+    familyName: ["Smith","Davis"]
 };
--- sampleProject.js
+++ sampleProject.js
@@ -1,8 +1,8 @@
 module.exports = {
     basePrompt: "A {emotion} girl named {name} with purple hair",
     negative_prompt: "AC_Neg1",
-    steps: 30,
-    guidance_scale: [4,6],
+    steps: 20,
+    guidance_scale: 4,
     seed: -1
     
 };
