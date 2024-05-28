8c6ae0ae03ca9e36eb4408fefc617e07e9ef24a0
Ben Davies <bendavies@he3.com.au>
2024-05-28 22:51:18 +1000

    Refactored to add generations data structure and numerous resulting bug fixes

--- sampleProject.js
+++ sampleProject.js
@@ -2,6 +2,7 @@ module.exports = {
     basePrompt: "A {emotion} girl named {name} with purple hair",
     negative_prompt: "AC_Neg1",
     steps: 30,
-    guidance_scale: 4
+    guidance_scale: [4,6],
+    seed: -1
     
 };
