02dfdbed4e1f944f40cc44a63aa7c9f7c35be93f
Ben Davies <bendavies@he3.com.au>
2024-05-31 11:59:30 +1000

    Fixed handling of project configuration

--- sampleProject.js
+++ sampleProject.js
@@ -1,8 +1,8 @@
 module.exports = {
     projectName: "SampleProject",
-    basePrompt: "A {ethnicity} person happy that the default prompt has been changed",
+    prompt: "A {ethnicity} person happy that the default prompt has been changed",
     negative_prompt: "AC_Neg1",
-    steps: [20,30],
+    // steps: [20,30],
     // guidance_scale: 4,
     // seed: -1
     
